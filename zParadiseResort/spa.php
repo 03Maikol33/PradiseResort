df9725a1d68994794bdd87aab526a34e4eaa403d
Chiara Zanin <sheischiary@gmail.com>
2026-07-12 00:52:28 +0200

    implementazione sezione piscine

---
+++ zParadiseResort/spa.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__ . '/include/bootstrap.inc.php';
+
+$skin = new_page($config['skin']);
+$skin->setContent('title',      'Piscine & SPA');
+$skin->setContent('year',       date('Y'));
+$skin->setContent('base',       $config['base']);
+$skin->setContent('skin',       $config['skin']);
+$skin->setContent('is_logged',  !empty($_SESSION['user']) ? '1' : '');
+$skin->setContent('user.name',  $_SESSION['user']['name'] ?? '');
+$cartCountVal = get_cart_count();
+$skin->setContent('cart_count', $cartCountVal > 0 ? '1' : '');
+$skin->setContent('cart_badge', $cartCountVal > 0 ? " ($cartCountVal)" : '');
+
+$block = new_block('spa');
+$block->setContent('base', $config['base']);
+$block->setContent('skin', $config['skin']);
+
+$skin->setContent('body', $block->get());
+$skin->close();
